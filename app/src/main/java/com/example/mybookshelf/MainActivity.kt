e203790d80d3bcce02a3628ffc089bae57b33f6e
Glenn Halls <gjhalls@gmail.com>
2023-09-04 17:50:22 +1000

    Add: application kt

--- app/src/main/java/com/example/mybookshelf/MainActivity.kt
+++ app/src/main/java/com/example/mybookshelf/MainActivity.kt
@@ -3,16 +3,13 @@ package com.example.mybookshelf
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.mybookshelf.ui.MyBookshelfScreen
-import com.example.mybookshelf.ui.theme.MyBookshelfTheme
+import com.example.mybookshelf.ui.MyBookshelfApplication
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyBookshelfTheme {
-                MyBookshelfScreen()
-            }
+            MyBookshelfApplication()
         }
     }
 }
---
+++ app/src/main/java/com/example/mybookshelf/ui/MyBookshelfApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mybookshelf.ui
+
+import androidx.compose.runtime.Composable
+import com.example.mybookshelf.ui.theme.MyBookshelfTheme
+
+@Composable
+fun MyBookshelfApplication() {
+    MyBookshelfTheme {
+        MyBookshelfScreen()
+    }
+}
