e69148b86fe45c3242d3945f140ac52ff94e1883
Glenn Halls <gjhalls@gmail.com>
2023-09-05 07:19:12 +1000

    Add: window state utilities

---
+++ app/src/main/java/com/example/mybookshelf/ui/util/WindowStateUtils.kt
@@ -0,0 +1,9 @@
+package com.example.mybookshelf.ui.util
+
+enum class BookshelfNavigationType {
+    BOTTOM_BAR, NAVIGATION_RAIL, NAVIGATION_DRAWER
+}
+
+enum class BookshelfContentLayout {
+    LIST_ONLY, DETAILS_ONLY, LIST_AND_DETAILS
+}
