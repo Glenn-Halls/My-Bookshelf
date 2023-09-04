b2fda326d4e45c2f4fa30347701bf90f3d364393
Glenn Halls <gjhalls@gmail.com>
2023-09-04 17:27:40 +1000

    Add: top app bar

---
+++ app/src/main/java/com/example/mybookshelf/ui/MyBookshelfScreen.kt
@@ -0,0 +1,81 @@
+package com.example.mybookshelf.ui
+
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mybookshelf.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyBookshelfScreen(
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            MyBookshelfTopBar {}
+        }
+    ){ paddingValues ->
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) {
+            Text(
+                text = "Test",
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyBookshelfTopBar(onUpButtonClick: () -> Unit) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+        ),
+        navigationIcon = {
+            IconButton(onClick = onUpButtonClick) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.back_button)
+                )
+            }
+        }
+    )
+}
+
+@Composable
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+)
+fun MyBookshelfScreenPreview(){
+    MyBookshelfScreen()
+}
