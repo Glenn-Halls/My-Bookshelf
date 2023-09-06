caefd08dd1eda181468bb87cbabcbbe0f4c6a1bb
Glenn-Halls <ghalls.dev@gmail.com>
2023-09-06 14:33:14 +1000

    Add: layout adaptation tests

--- app/src/test/java/com/example/mybookshelf/model/test/BookshelfViewModelTest.kt
+++ app/src/test/java/com/example/mybookshelf/model/test/BookshelfViewModelTest.kt
@@ -1,14 +1,27 @@
 package com.example.mybookshelf.model.test
 
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import com.example.mybookshelf.model.BookshelfViewModel
+import com.example.mybookshelf.ui.util.BookshelfNavigationType
 import com.example.mybookshelf.ui.util.NavigationTabs
 import com.example.mybookshelf.ui.util.ScreenSelect
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 class BookshelfViewModelTest {
+    // Instance of viewModel to be used for testing
     private val viewModel = BookshelfViewModel()
+
+    // Instances of window size classes representing common device configurations
+    private val mobilePhonePortrait = WindowSizeClass.calculateFromSize(DpSize(450.dp, 800.dp))
+    private val mobilePhoneLandscape = WindowSizeClass.calculateFromSize(DpSize(800.dp, 450.dp))
+    private val foldablePortrait = WindowSizeClass.calculateFromSize(DpSize(700.dp, 900.dp))
+    private val foldableLandscape = WindowSizeClass.calculateFromSize(DpSize(900.dp, 700.dp))
 
     @Test
     fun viewModel_initialValue_screenSelectIsnull() {
@@ -93,5 +106,39 @@ class BookshelfViewModelTest {
         val currentSelectedScreen = viewModel.uiState.value.currentScreen
 
         assertEquals(favouritesScreen, currentSelectedScreen)
+    }
+
+
+
+    @Test
+    // Get Bottom Bar navigation for portrait mobile phone
+    fun viewModel_getNavigationSetup_BottomBarNavigation() {
+        val bottomBarNavigation = BookshelfNavigationType.BOTTOM_BAR
+        val currentNavigation = viewModel.getNavigationSetup(mobilePhonePortrait)
+        assertEquals(bottomBarNavigation, currentNavigation)
+    }
+
+    @Test
+    // Get Navigation Rail for landscape mobile phone
+    fun viewModel_getNavigationSetup_NavigationRail() {
+        val navigationRail = BookshelfNavigationType.NAVIGATION_RAIL
+        val currentNavigation = viewModel.getNavigationSetup(mobilePhoneLandscape)
+        assertEquals(navigationRail, currentNavigation)
+    }
+
+    @Test
+    // Get Navigation Drawer for landscape foldable
+    fun viewModel_getNavigationSetup_NavigationDrawer() {
+        val navigationDrawer = BookshelfNavigationType.NAVIGATION_DRAWER
+        val currentNavigation = viewModel.getNavigationSetup(foldableLandscape)
+        assertEquals(navigationDrawer, currentNavigation)
+    }
+
+    @Test
+    // Get Navigation Rail for portrait foldable
+    fun viewModel_getNavigationSetup_FoldablePortrait() {
+        val navigationRail = BookshelfNavigationType.NAVIGATION_RAIL
+        val currentNavigation = viewModel.getNavigationSetup(foldablePortrait)
+        assertEquals(navigationRail, currentNavigation)
     }
 }
