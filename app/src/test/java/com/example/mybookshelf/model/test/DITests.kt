7994097455f7328a5baf7921a91fc72c46b136ed
Glenn Halls <gjhalls@gmail.com>
2023-09-12 15:37:44 +1000

    Add: test dispatchers

---
+++ app/src/test/java/com/example/mybookshelf/model/test/DITests.kt
@@ -0,0 +1,54 @@
+package com.example.mybookshelf.model.test
+
+import com.example.mybookshelf.fake.FakeNetworkBestsellerRepository
+import com.example.mybookshelf.fake.FakeNetworkBookRepository
+import com.example.mybookshelf.model.BookshelfViewModel
+import com.example.mybookshelf.rules.TestDispatcherRule
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+class DITests {
+
+    val createViewModel = {
+        BookshelfViewModel(
+            bookRepository = FakeNetworkBookRepository(),
+            bestsellerRepository = FakeNetworkBestsellerRepository()
+        )
+    }
+
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
+
+    @Test
+    fun dispatcherTest() =
+        runTest {
+            val one = 1
+            assertEquals(1, one)
+        }
+
+    @Test
+    fun testViewModel() =
+        runTest {
+            val viewModel = createViewModel()
+            assertEquals(
+                viewModel.uiState.value.searchResult,
+                "Success, 3 books found."
+            )
+        }
+
+
+    @Test
+    fun viewModelTest() =
+        runTest {
+            val viewModel = BookshelfViewModel(
+                FakeNetworkBookRepository(),
+                FakeNetworkBestsellerRepository()
+            )
+            assertEquals(
+                viewModel.uiState.value.searchResult,
+                "Success, 3 books found."
+            )
+        }
+}
