3ea394df79e17541dac5ce25d60b1363a121b93d
Glenn Halls <gjhalls@gmail.com>
2023-09-15 16:07:23 +1000

    Add: NYT API key

--- app/src/main/java/com/example/mybookshelf/network/BookshelfApiService.kt
+++ app/src/main/java/com/example/mybookshelf/network/BookshelfApiService.kt
@@ -4,6 +4,6 @@ import com.example.mybookshelf.model.BookSearchResult
 import retrofit2.http.GET
 
 interface BookshelfApiService {
-    @GET ("books/v1/volumes?q=jazz+history&maxResults=40")
+    @GET ("books/v1/volumes?q=jazz+history&maxResults=40&key=AIzaSyBYvGg2AAjaTh91NSM8evzlLHWHTuZ3_R0")
     suspend fun getBooks(): BookSearchResult
 }
